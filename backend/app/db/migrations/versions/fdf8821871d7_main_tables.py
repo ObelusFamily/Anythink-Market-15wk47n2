bdc3bf1882738b02b09664169be7f07316f60282
tunafizz <121314523+tunafizz@users.noreply.github.com>
2022-12-24 16:33:11 +0000

    ,

--- backend/app/db/migrations/versions/fdf8821871d7_main_tables.py
+++ backend/app/db/migrations/versions/fdf8821871d7_main_tables.py
@@ -60,7 +60,7 @@ def create_users_table() -> None:
         sa.Column("hashed_password", sa.Text),
         sa.Column("bio", sa.Text, nullable=False, server_default=""),
         sa.Column("image", sa.Text),
-        sa.Column("is_verified", sa.Boolean, nullable=False)
+        sa.Column("is_verified", sa.Boolean, nullable=False),
         *timestamps(),
     )
     op.execute(
